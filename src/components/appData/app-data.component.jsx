7abaa980d686823ba98f0de452edda10e6f525e6
Allegra Megan Anka <allegra.anka@gmail.com>
2020-02-04 18:33:54 -0500

    app data component progress

--- src/components/appData/app-data.component.jsx
+++ src/components/appData/app-data.component.jsx
@@ -3,8 +3,17 @@ import './app-data.styles.css';
 
 export const AppData = props => (
     <div className='app-data-container'>
-        <div>
-            <h1> {props.appData.aid} </h1>
+        <div className='app-basic-data1'>
+            <h1><span>{props.appData.aid}</span></h1>
+            <p><span>{props.appData.name}</span></p>
+            <p><span>api token: {props.appData.api_token}</span></p>
+            <p><span>private key: {props.appData.private_key}</span></p>
+        </div>
+        <div className='app-basic-data2'>
+            <p>user provider: {props.appData.user_provider}</p>
+            <p>app state: {props.appData.state}</p>
+            <p>site: {props.appData.url}</p>
+
         </div>
     </div>
 );
